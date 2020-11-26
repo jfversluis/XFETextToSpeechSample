b2e848b2da837bc327c4af004b2e9229486c2ec4
Gerald Versluis <gerald@verslu.is>
2020-11-26 10:12:41 +0100

    Implement TTS

--- XFETextToSpeechSample/MainPage.xaml.cs
+++ XFETextToSpeechSample/MainPage.xaml.cs
@@ -5,14 +5,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials;
 
 namespace XFETextToSpeechSample
 {
     public partial class MainPage : ContentPage
     {
+        IEnumerable<Locale> locales;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            locales = await TextToSpeech.GetLocalesAsync();
+
+            foreach (var l in locales)
+                Languages.Items.Add(l.Name);
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            if (Languages.SelectedIndex > 0)
+            {
+                TextToSpeech.SpeakAsync(TextToSpeak.Text, new SpeechOptions
+                {
+                    Locale = locales.Single(l => l.Name == Languages.SelectedItem.ToString())
+                });
+            }
         }
     }
 }
